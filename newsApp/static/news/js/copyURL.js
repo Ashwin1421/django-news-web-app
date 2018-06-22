5fea1ce1c5ad4798568404ee0da5ff0673a8fb29
Ashwin Joshi <ashwin.joshi12@live.com>
2018-06-22 17:24:12 -0500

    Updated views.py

--- newsApp/static/news/js/copyURL.js
+++ newsApp/static/news/js/copyURL.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
 
     setTimeout(function() {$('#clipboardModal').modal('hide');$('#shareModalDialog').modal('hide');}, 4000);
+    $('#shareUrl').popover();
+
+    $('.social-share-icon').click(function(){
+        window.open(
+            this.href,
+            '', 
+            'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600'
+        );
+        return false;
+    });
 });
